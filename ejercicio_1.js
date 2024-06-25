9175302d13b59bb246bb9caefb497096d3937b5d
gatonegro1977 <148931069+gatonegro1977@users.noreply.github.com>
2024-06-24 23:45:30 -0300

    Add files via upload

---
+++ ejercicio_1.js
@@ -0,0 +1,47 @@
+"use strict";
+
+/* Inove Coding School
+ * Tarea! Ejercicios básicos de condicionales.
+ */
+
+// Ejercicios de práctica numérica
+// Comparadores
+// Ingrese dos números cualesquiera y realice las sigueintes
+// comparaciones entre ellos
+const numero1 = parseInt(prompt('Ingrese el primer número:\n'));
+
+const numero2 = parseInt(prompt('Ingrese el segundo número:\n'));
+
+// Compare cual de los dos números es mayor
+// Imprima en pantalla según corresponda
+if(numero1> numero2) {
+   console.log("numero1 es mayor a numero 2")
+} else {
+   console.log("numero 2 es mayor a numero1")
+}
+    
+
+if (numero1 > 0) {
+   console.log("numero 1 es +")
+} else if (numero1 < 0) {
+   console.log(numero1 es = a 0)   
+} else {
+   console.log(numero1 es = a 0)
+}
+   
+
+
+
+
+
+
+
+// Verifique si el numero1 positivo, negativo o cero
+// Imprima el resultado en cada caso
+
+// Verifique si el numero1 es mayor a 0 y menor a 100
+// Imprima en pantalla si se cumple o no la condición
+
+// Verifique si el numero1 es menor a 10 o el numero2
+// es mayor a - 2
+// Imprima en pantalla si se cumple o no la condición
